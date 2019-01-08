270428b5c33a91bdfa182f654b01bfa6bbb70bd2
Ashton Honnecke <ashton@digitalassetsdata.com>
2019-01-08 12:20:18 -0700

    cleanup isort file a bit

--- personal/isort.el
+++ personal/isort.el
@@ -1,13 +1,18 @@
-;;(require 'py-isort)
+;;This is a great example/template for executing arbitrary code on the current buffer from an external
+;;executable, suitable for linting, formatting, any automated tools that will "fix" the file
 
-;; (add-hook 'python-mode-hook
-;;  (lambda ()(add-hook 'before-save-hook 'py-isort-before-save nil 'local)))
-
+;; Run isort against the current file and revert the buffer
 (defun isort-imports ()
-  "run a command on the current file and revert the buffer"
+  "run isort on the current file and revert the buffer"
   (interactive)
   (shell-command
    (format "pipenv run isort %s"
            (shell-quote-argument (buffer-file-name))))
   (revert-buffer t t t))
 (global-set-key (kbd "M-s-^") 'isort-imports)
+
+
+;; Add a hook that auto runs isort on save for all python files
+(add-hook 'python-mode-hook
+          (lambda ()(add-hook 'before-save-hook 'isort-imports nil 'local)))
+
