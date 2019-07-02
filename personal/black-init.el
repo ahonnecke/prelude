56460070d63a83882bf78fcc4311ea762de8ac49
Ashton Honnecke <ahonneck@gmail.com>
2019-07-02 10:50:13 -0600

    stuff

--- personal/black-init.el
+++ personal/black-init.el
@@ -6,5 +6,13 @@
            (shell-quote-argument (buffer-file-name))))
   (revert-buffer t t t))
 
-(add-hook 'before-save-hook 'blacken-buffer)
+;; (add-hook 'before-save-hook 'blacken-buffer)
 (add-hook 'python-mode-hook 'blacken-mode)
+
+;; TODO make this work
+(defun my-set-tab-mode ()
+  (when (and (stringp buffer-file-name)
+             (string-match "\\.py\\'" buffer-file-name))
+    (add-hook 'before-save-hook 'blacken-buffer)))
+
+(add-hook 'python-mode-hook 'my-set-tab-mode)
