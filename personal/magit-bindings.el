c43a64f37d757aa0bff804b6be9cf06676c41737
Ashton Honnecke <ahonneck@gmail.com>
2019-12-07 11:30:24 -0700

    use better ffap

--- personal/magit-bindings.el
+++ personal/magit-bindings.el
@@ -52,7 +52,52 @@
                     ;; expand-file-name fixes "~/~/.emacs" bug sent by CHUCKR.
                     (expand-file-name filename)))
           ;; User does not want to find a non-existent file:
-          ((signal 'file-error (list "Opening filee buffer"
+          ((signal 'file-error (list "Opening file buffer"
+                                     "no such file or directory"
+                                     filename))))))
+
+(defun find-line-hook-failure-at-point (&optional filename)
+  "Opens file at point and moves point to line specified next to file name."
+  (interactive)
+  (let* ((filename (or filename (if current-prefix-arg (ffap-prompter) (ffap-guesser))))
+         (line-number
+          (and (or (looking-at ".* line \\(\[0-9\]+\\)")
+                   (looking-at "[^:]*:\\(\[0-9\]+\\)"))
+               (string-to-number (match-string-no-properties 1))))
+         (column-number
+          (or
+           (and (looking-at "[^:]*:\[0-9\]+:\\(\[0-9\]+\\)")
+                (string-to-number (match-string-no-properties 1)))
+           (let 'column-number 0))))
+    (message "%s --> %s:%s" filename line-number column-number)
+    (cond ((ffap-url-p filename)
+           (let (current-prefix-arg)
+             (funcall ffap-url-fetcher filename)))
+          ((and line-number
+                (file-exists-p filename))
+           (progn (find-file-other-window filename)
+                  ;; goto-line is for interactive use
+                  (goto-char (point-min))
+                  (forward-line (1- line-number))
+                  (forward-char column-number)))
+          ((and ffap-pass-wildcards-to-dired
+                ffap-dired-wildcards
+                (string-match ffap-dired-wildcards filename))
+           (funcall ffap-directory-finder filename))
+          ((and ffap-dired-wildcards
+                (string-match ffap-dired-wildcards filename)
+                find-file-wildcards
+                ;; Check if it's find-file that supports wildcards arg
+                (memq ffap-file-finder '(find-file find-alternate-file)))
+           (funcall ffap-file-finder (expand-file-name filename) t))
+          ((or (not ffap-newfile-prompt)
+               (file-exists-p filename)
+               (y-or-n-p "File does not exist, create buffer? "))
+           (funcall ffap-file-finder
+                    ;; expand-file-name fixes "~/~/.emacs" bug sent by CHUCKR.
+                    (expand-file-name filename)))
+          ;; User does not want to find a non-existent file:
+          ((signal 'file-error (list "Opening file buffer"
                                      "no such file or directory"
                                      filename))))))
 
@@ -63,7 +108,7 @@
 
 (advice-add 'magit-process-insert-section :before #'auto-display-magit-process-buffer)
 
-(define-key magit-process-mode-map  (kbd "<return>") 'find-file-at-point)
+
 
 ;;o
 ;;(global-set-key (kbd "H-o") 'find-file-at-point)
--- personal/z-dad-init.el
+++ personal/z-dad-init.el
@@ -3,6 +3,8 @@
 
 (bookmark-bmenu-list)
 
+(define-key magit-process-mode-map  (kbd "<return>") 'find-line-hook-failure-at-point)
+
 (use-package fzf
   ;; Quick way to jump to a given char.
   :bind ("H-f" . fzf-git))
